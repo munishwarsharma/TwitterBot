0339499755e6a64b12fc34c9980f5ac7639bb27d
munishwarsharma <109985301+munishwarsharma@users.noreply.github.com>
2022-07-26 14:07:04 +0530

    Update k1.js

--- k1.js
+++ k1.js
@@ -1,6 +1,6 @@
 module.exports = {
-    appKey:         'fHWf9mcKtqwwnQZ1DAVHpfB68',
-    appSecret:      '2n4YUpkf2MyajB6CeR8WN6QOCqlglt2bJtQ1T26IcOM20bX9un',
-    accessToken:        '1551197749726482434-ho6HR8VbnLsyZApxRwab6twF66MouE',
-    accessSecret:  'g4desPHbT3YYEVed9ROm1csI1A7QjSyQD2HDmcWpDaFAK'
+    appKey:         '...',
+    appSecret:      '...',
+    accessToken:        '...',
+    accessSecret:  '...'
 };
